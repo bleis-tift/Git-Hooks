36b029bd9724f2693719f987d29ea9beeecde875
clairvy <clairvy@gmail.com>
2011-07-08 12:39:12 +0900

    zsh で補完するのに少し設定が必要だったので，Formula にmessage を追加

--- git-hooks.rb
+++ git-hooks.rb
@@ -15,5 +15,13 @@ class GitHooks < Formula
     puts '# BEGIN example. (write into $HOME/.bashrc if use bash)'
     puts 'GIT_HOOKS_HOME=/usr/local/share/git-core/Git-Hooks; export GIT_HOOKS_HOME'
     puts '# END'
+
+    ohai 'If you use zsh and want to completion git hooks, write bellow setting in .zshrc'
+    puts '# BEGIN use git hooks completion'
+    puts 'source /usr/local/etc/bash_completion.d/git-hooks-completion.bash'
+    puts '# END'
+
+    ohai 'and run bellow'
+    puts '$ git config --global alias.hooks hooks'
   end
 end
